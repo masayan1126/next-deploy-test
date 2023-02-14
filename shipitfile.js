3ecfc51f3ed5d5b01b5fccc66119b4305659b1a6
nishigaki <nishigaki@aivick.co.jp>
2023-02-14 12:25:23 +0900

    a

--- shipitfile.js
+++ shipitfile.js
@@ -35,6 +35,7 @@ module.exports = (shipit) => {
     shipit.blTask('npm:start', async () => {
         await shipit.log(`deploying to ...`)
         await shipit.remote(`cd /opt/deploy-test/current`)
+        await shipit.remote(`nvm install 16`)
         await shipit.remoteCopy("package.json", "/opt/deploy-test/current/")
         await shipit.remoteCopy("package-lock.json", "/opt/deploy-test/current/")
         await shipit.remote("~/.nvm/versions/node/v16.15.1/bin/npm install --production")
