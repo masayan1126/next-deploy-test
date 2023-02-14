2226225e7989d853a6192150b5d225ef022cd255
nishigaki <nishigaki@aivick.co.jp>
2023-02-14 09:46:02 +0900

    Add

---
+++ shipitfile.js
@@ -0,0 +1,40 @@
+module.exports = (shipit) => {
+    const shipDeploy = require('shipit-deploy')
+
+    shipDeploy(shipit)
+
+    shipit.initConfig({
+    default: {
+        workspace: '.',
+        repositoryUrl: false,
+        shallowClone: false,
+        keepWorkspace: true,
+    },
+
+    servers: {
+        host: 'ec2-13-230-136-66.ap-northeast-1.compute.amazonaws.com',
+        port: 22,
+        user: 'ec2-user',
+        extraSshOptions: {
+            StrictHostKeyChecking: 'no',
+            UserKnownHostsFile: '/dev/null',
+        },
+        },
+        branch: 'origin/develop',
+        deployTo: '/opt/deploy-test',
+        rsyncFrom: './.next',
+        keepReleases: 3,
+        key: '~/.ssh/ssh_key',
+    })
+
+    shipit.on('fetched', async () => {
+        shipit.start('npm:start')
+    })
+
+    shipit.blTask('npm:start', async () => {
+        await shipit.remote(`cd /opt/deploy-test/current`)
+        .then(() => {
+            shipit.remote("npm install && npm run build && npm run start")
+        })
+    })
+}
