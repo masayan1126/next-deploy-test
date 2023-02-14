e6660d637c5562c8c093ac27978115838f4ae067
nishigaki <nishigaki@aivick.co.jp>
2023-02-14 10:12:07 +0900

    a

--- shipitfile.js
+++ shipitfile.js
@@ -11,23 +11,22 @@ module.exports = (shipit) => {
         keepWorkspace: true,
     },
     staging: {
-        servers: {
-            host: 'ec2-13-230-136-66.ap-northeast-1.compute.amazonaws.com',
-            port: 22,
-            user: 'ec2-user',
-            extraSshOptions: {
-                StrictHostKeyChecking: 'no',
-                UserKnownHostsFile: '/dev/null',
-            },
-            },
-                branch: 'origin/develop',
-                deployTo: '/opt/deploy-test',
-                rsyncFrom: './.next',
-                keepReleases: 3,
-                key: '~/.ssh/ssh_key',
-            }
-        })
-    }
+    servers: {
+        host: 'ec2-13-230-136-66.ap-northeast-1.compute.amazonaws.com',
+        port: 22,
+        user: 'ec2-user',
+        extraSshOptions: {
+            StrictHostKeyChecking: 'no',
+            UserKnownHostsFile: '/dev/null',
+        },
+        },
+            branch: 'origin/develop',
+            deployTo: '/opt/deploy-test',
+            rsyncFrom: './.next',
+            keepReleases: 3,
+            key: '~/.ssh/ssh_key',
+        }
+    })
 
     shipit.on('fetched', async () => {
         shipit.start('npm:start')
