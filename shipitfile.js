d71f1849f8d4ff94664c375a046649b7f3a50bf8
nishigaki <nishigaki@aivick.co.jp>
2023-02-15 14:34:57 +0900

    あ

--- shipitfile.js
+++ shipitfile.js
@@ -46,9 +46,10 @@ module.exports = (shipit) => {
 
         await shipit.log(`start next server ...`)
         await shipit.remote(`ln -s /opt/deploy-test/shared/.env.prod /opt/deploy-test/current/.env.prod`)
+        // pm2はグローバルインストールする(×node_modules/.bin/pm2)
         // cp env
         // 初回起動：node_modules/.bin/pm2 start npm --name "next" -- start
-        // await shipit.remote(`cd /opt/deploy-test/current && nvm install 16 && npm install --production && npm run build && node_modules/.bin/pm2 restart next`)
-        await shipit.remote(`cd /opt/deploy-test/current && nvm install 16 && npm install --production && node_modules/.bin/pm2 kill && npm run build:prod && node_modules/.bin/pm2 start npm --name "next" -- start`)
+        // await shipit.remote(`cd /opt/deploy-test/current && nvm install 16 && npm install --production && npm run build && pm2 restart next`)
+        await shipit.remote(`cd /opt/deploy-test/current && nvm install 16 && npm install --production && pm2 kill && npm run build:prod && pm2 start npm --name "next" -- start`)
     })
 }
