f8c388182f427c934c61bdb1ea68ffb73b52bb1d
nishigaki <nishigaki@aivick.co.jp>
2023-02-16 15:20:35 +0900

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        aaaaa
+        c1
+        c2
         このサーバーは{process.env.NEXT_PUBLIC_ENVIRONMENT}環境です
         <button onClick={() => router.push("/about")}>１．ルーティングテスト</button>
       </main>
